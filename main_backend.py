e16968923eb86cdb9b07ecebd7d71020eeef83d4
Dragomir <dragomir91@hotmail.fr>
2023-09-11 16:52:10 +0200

    modification dans la fonction def info_client (main_backend.py) V14

--- main_backend.py
+++ main_backend.py
@@ -77,8 +77,10 @@ async def info_client():
     df_kernel = load_df()
     #info = List_id(id_client =df_kernel.SK_ID_CURR,information_client =df_kernel.columns)
     #print(info)
-    return {"liste id" :[df_kernel.loc[:101,'SK_ID_CURR']],
-            "info_client" : [df_kernel.columns[:101]]}
+    df = {"liste id" :df_kernel.loc[:101,'SK_ID_CURR'],
+            "info_client" : df_kernel.columns[:101]}
+    print(pd.DataFrame(df))
+    return pd.DataFrame(df)
 
 @app.post("/info_client/id")
 async def info_client2(info_id: Info_id):
