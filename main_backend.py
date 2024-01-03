15c2faa21d58d8b395811edbef02b1bc82d64433
Dragomir <49881674+Dragomir91@users.noreply.github.com>
2024-01-03 16:52:28 +0100

    Update main_backend.py
    update comments

--- main_backend.py
+++ main_backend.py
@@ -53,6 +53,7 @@ class Info_id(BaseModel):
 
 app = FastAPI()
 
+##########################AFFICHE_LA_PAGE_DU_SERVEUR##########################
 @app.get("/")
 async def root():
     
@@ -69,8 +70,9 @@ async def root():
     """
       
     return HTMLResponse(content=html_content, status_code=200)
-#######################HTML_RESPONSE#################################################
-
+    
+    
+########################ID_CLIENT########################
 @app.get("/id_client")  
 async def id_client():
     df_kernel = load_df()
@@ -78,22 +80,20 @@ async def id_client():
 
     return {"list_id" : list(df_kernel.loc[:200,'SK_ID_CURR']),
             "infos_id" : list(df_kernel.columns[:200])}
-########################ID_CLIENT#######################################################
+    
 
+########################INFORMATION_CLIENT########################
 @app.get("/info_client")
 async def info_client(info_id: Info_id):
     
     df_kernel = load_df()
     #print('id_client = ',  info_id.id_client)
     #print('info_client = ',  info_id.infos_id)
-    df_id = df_kernel.loc[df_kernel['SK_ID_CURR'] == info_id.id_client,info_id.infos_id]
-
-    #print('df_id = ',df_id)  
+    df_id = df_kernel.loc[df_kernel['SK_ID_CURR'] == info_id.id_client,info_id.infos_id] 
     return df_id.to_json()
 
 
-########################ID_CLIENT#######################################################
-
+########################AFFICHE_LES_DONNEES_CLIENTS########################
 @app.get("/info_clients/id_all")
 async def info_client2(info_id: Info_id):
     
@@ -101,29 +101,21 @@ async def info_client2(info_id: Info_id):
     print('id_client = ',  info_id.id_client)
     print('info_client = ',  info_id.infos_id)
     df_id = df_kernel.loc[:20,info_id.infos_id]
-
     print('df_id = ',df_id)  
     return df_id.to_json()
+    
 
-####################################PREDICT_ID#############################################
-
+########################PREDICTION_DEMANDE_ENPRUNT_CLIENT########################
 @app.post("/predict") 
 async def predict_decision(pred_id : Predict_id):
 
     df_kernel = load_df()
 
     base_dir = "" 
+    # chargement du modèle LightGBM
     csv_file_path = os.path.join(base_dir, "model_lightgbm.pkl")
-
-    
     with open(csv_file_path,'rb') as f:
-            model_lightgbm = pickle.load(f)
-    print(pred_id)       
-    
-    print(df_kernel)
-    
-
-
+            model_lightgbm = pickle.load(f)    
     cols= [
         'EXT_SOURCE_2',
         'EXT_SOURCE_3',
@@ -140,23 +132,15 @@ async def predict_decision(pred_id : Predict_id):
         'AMT_GOODS_PRICE']  
     
     df = df_kernel.loc[:,:]
-
-    print('df shape : ',df.shape)    
-
     X = df[df.SK_ID_CURR == pred_id.id].loc[:,cols]
-    print('x = ',X)
-
     y_pred = model_lightgbm.predict(X)
-    
+    # calcul la probabilité de remboursement du credit
     y_pred_proba = model_lightgbm.predict_proba(X)[0]
-    print('proba id : ', y_pred_proba)
     info_id = Predict_id(id=pred_id.id, decision_id = y_pred, proba=y_pred_proba[0])
-    print(info_id)
-
     return info_id
 
-#############################################info_detail_client########################################
 
+########################détail_client########################
 @app.get("/predict/feature_id") 
 async def predict_decision(ex_id : Predict_id):
 
@@ -171,7 +155,7 @@ async def predict_decision(ex_id : Predict_id):
     df_kernel = df_kernel.fillna(0)
     df_kernel = df_kernel.replace([np.inf, -np.inf], np.nan)
 
-    base_dir = ""  # Vous devrez peut-être adapter ce chemin en fonction de votre configuration
+    base_dir = "" 
 
     # Chemin absolu complet du fichier CSV
     csv_file_path = os.path.join(base_dir, "model_lightgbm_shap.pkl")
@@ -180,8 +164,6 @@ async def predict_decision(ex_id : Predict_id):
     with open(csv_file_path,'rb') as f:
             shap_values = pickle.load(f)
             
- 
-
     mean_abs_shap_values = shap_values.values.sum(axis = 0)
 
     # Trier les variables par ordre d'importance décroissante
@@ -195,7 +177,6 @@ async def predict_decision(ex_id : Predict_id):
 
     print(df_kernel.loc[df_kernel.SK_ID_CURR == ex_id.id,feature_names[:15]])
     
-  
     cols= ['EXT_SOURCE_2',
            'EXT_SOURCE_3',
            'DAYS_BIRTH',
@@ -214,8 +195,7 @@ async def predict_decision(ex_id : Predict_id):
     return df_kernel.loc[df_kernel.SK_ID_CURR==ex_id.id,cols].to_json()
 
 
-
-#########################################EXPLAIN_SHAP_VALUES###########################################
+########################EXPLAIN_SHAP_VALUES########################
 
 @app.get("/predict/explain") 
 async def predict_decision(explain_id : Predict_id):
@@ -254,9 +234,7 @@ async def predict_decision(explain_id : Predict_id):
             "feature_names" :shap_values.feature_names}
 
 
-#########################################COUT_METIER############################################
-
-
+########################PERTE_ARGENT########################
 def perte(dataframe, y_true, y_pred,col_credit, rate_default:float):
     
     df = {'Index':y_true.index,'y_pred':y_pred,'Target':y_true.values}
@@ -265,24 +243,24 @@ def perte(dataframe, y_true, y_pred,col_credit, rate_default:float):
     dataframe['perte'] = -dataframe.loc[idx,col_credit] * rate_default
     return dataframe
 
+
+########################GAIN_ARGENT########################
 def gain(df, y_proba, threshold:float):
-        
         
         df['proba'] = y_proba[:,0]
         df['gain'] = 0
         df['y_pred'] = np.nan
-        
         #df['gain'] = df.loc[(df.proba >= threshold), 'AMT_CREDIT'] * rate_interest
         df.loc[(df.proba >= threshold), 'y_pred'] = 0
         df.loc[(df.proba < threshold), 'y_pred'] = 1
-            
                 
         return df['gain']
 
+
+########################DONNEE_CLIENT########################
 @app.get("/predict/graph_id") 
 async def predict_decision(cout_id : Predict_id):
-    
-    
+        
     df_test = load_df()    
     df_kernel = load_df_kernel()
     cols= ['SK_ID_CURR',
@@ -320,11 +298,7 @@ async def predict_decision(cout_id : Predict_id):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
     df_sample = pd.DataFrame(df.loc[y_test.index])
-
-
-    print(df.shape)
     y_pred = model_lightgbm.predict(X)
-    print(len(y_pred))
     df = df_test.loc[:,cols]
     df['Y_PRED_PV_ID'] = y_pred
     df['EXT_SOURCE_2_PV_ID'] = X['EXT_SOURCE_2']
@@ -336,11 +310,12 @@ async def predict_decision(cout_id : Predict_id):
     print(df.iloc[:,10:])
     return  df.to_json()
 
+
+########################CHARGEMENT_DU_FICHIER_SUR_UN_PETIT_JEU_DE_DONNEE########################
 def load_df_kernel():
-    #df_app_test = pd.read_csv('source/application_test.csv',sep=',')
-    #df_app_test = pd.read_csv('source/application_test.csv',sep=',')
+
     # Chemin absolu vers le répertoire contenant le fichier CSV
-    base_dir = ""  # Vous devrez peut-être adapter ce chemin en fonction de votre configuratio
+    base_dir = ""  
  
     csv_file_path = os.path.join(base_dir, "kernel_light.csv")
 
@@ -352,13 +327,14 @@ def load_df_kernel():
         return  df
 
     else: 
-         return print("Le fichier df_kernel CSV n'existe pas.")  
-    
+         return print("Le fichier df_kernel CSV n'existe pas.")
+
+
+########################CHARGEMENT_DU_FICHIER_TEST########################  
 def load_df():
-    #df_app_test = pd.read_csv('source/application_test.csv',sep=',')
-    #df_app_test = pd.read_csv('source/application_test.csv',sep=',')
+
     # Chemin absolu vers le répertoire contenant le fichier CSV
-    base_dir = ""  # Vous devrez peut-être adapter ce chemin en fonction de votre configuratio
+    base_dir = ""  # 
  
     csv_file_path = os.path.join(base_dir, "app_test_light.csv")
 
@@ -373,6 +349,8 @@ def load_df():
     else: 
          return print("Le fichier df_kernel CSV n'existe pas.")  
 
+
+########################FOCNTION_TEST_AVEC_PYTEST########################
 def test_load_df():
     # Chemin absolu vers le répertoire contenant le fichier CSV
     base_dir = ""  
@@ -383,7 +361,7 @@ def test_load_df():
     assert isinstance(df, object)
     assert len(df) > 0  # Vérifier que le DataFrame n'est pas vide
     # Autres assertions selon votre cas d'utilisation
-# Exécutez pytest pour exécuter les tests
+
 
 if __name__ == '__main__':
     df_kernel = load_df()
